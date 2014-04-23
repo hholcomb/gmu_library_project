d04571714d8c8241e277b1f6d919a8869c109196
Howard Holcombe <sixkids7@earthlink.net>
2014-04-22 20:00:35 -0400

    initial rails struct

---
+++ library/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Library::Application.config.secret_key_base = 'f1211180fbe9316124ac9a3fdbc9f204991585ff56a91500a92e781658b984a52700e190d6dc0694f18211d4c90745fb0eaec4fbb99e0191fc0d661236ec3b8f'
