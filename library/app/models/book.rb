cbcd3b24e4d981e32d5777a7a4236c7e65108ea7
patricia_williams <williams_patricia@bah.com>
2014-05-06 19:59:07 -0400

    create new book feature

--- library/app/models/book.rb
+++ library/app/models/book.rb
@@ -1,2 +1,19 @@
 class Book < ActiveRecord::Base
+  
+GENRE = [ 'Sci-fi', 'Drama', 'Fiction' , 'Non-Fiction' ]
+
+  validates :title, :author, :isbn, :genre, :abstract, :pages,  presence: true
+  
+  validates :pages,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+      unless: "pages.blank?"
+  
+  validates :total_in_library,
+      numericality: { greater_than: 0 }
+
+  validates :genre, :inclusion => { :in => GENRE }
+
+  validates :abstract, length: { minimum: 15 }, 
+    unless: 'abstract.blank?'
+
 end
