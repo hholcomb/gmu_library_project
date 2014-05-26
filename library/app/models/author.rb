e7e7a55b90720687f4e1589b5170c7effb7202c4
Howard <sixkids7@earthlink.net>
2014-05-26 13:43:33 -0400

    initial working version of authors functionality

---
+++ library/app/models/author.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+gem 'activeresource'
+require 'active_resource'
+
+class Author < ActiveResource::Base
+  self.site = 'http://localhost:8080/gmulibrary/'
+  self.format = :json
+  self.headers['Accept'] = 'application/json'
+  schema do
+    integer 'id'
+    string 'name', 'biography', 'dob', 'nationality',
+           'awards', 'image_url'
+  end
+end
