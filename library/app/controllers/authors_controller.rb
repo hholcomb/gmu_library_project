9f1bfd51cf9c5e3b28b861f220d6a00414199fb1
Howard <sixkids7@earthlink.net>
2014-05-26 13:44:34 -0400

    initial working version of authors functionality - try #2

--- library/app/controllers/authors_controller.rb
+++ library/app/controllers/authors_controller.rb
@@ -1,15 +1,26 @@
 class AuthorsController < ApplicationController
-  before_action :set_author, only: [:show]
+#  before_action :set_author, only: [:show]
 
   # GET /authors
   # GET /authors.json
   def index
+    @authors = Author.all
   end
 
   # GET /authors/1
   # GET /authors/1.json
   def show
+    @authorid = params[:id]
+    @author = Author.find( params[:id] )
+    @publishedBooks = Book.where( "author = '#{@author.name}' " )
   end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_author
+      @author = Author.find(params[:id])
+    end
+
 
 
 end
