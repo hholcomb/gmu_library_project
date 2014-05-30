d2c120286989451f1190bde1d1c7409e92e3746e
Howard <sixkids7@earthlink.net>
2014-05-29 20:45:38 -0400

    working version of encrypted password

--- library/app/models/user.rb
+++ library/app/models/user.rb
@@ -1,16 +1,6 @@
 class User < ActiveRecord::Base
 
-	   # Returns self if the password is correct, otherwise false.
-	   # from https://github.com/rails/rails/blob/3-2-stable/activemodel/lib/active_model/secure_password.rb
-  def authenticate(login_password)
-    puts "user object fields are"
-    puts self.inspect
-        if  login_password == self.password
-          self
-        else
-          false
-        end
-  end
+has_secure_password
 
   #def password=(unencrypted_password)
   #      @password = unencrypted_password
@@ -19,8 +9,11 @@ class User < ActiveRecord::Base
   #      end
   #end
 
+#validates :password, presence: true, uniqueness: { case_sensitivity: false }
+
 has_many :books, through: :reservations, dependent: :destroy
 
 has_many :reservations
+
 
 end
--- library/db/schema.rb
+++ library/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526202701) do
+ActiveRecord::Schema.define(version: 20140530001214) do
 
   create_table "books", force: true do |t|
     t.string   "isbn"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20140526202701) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "password_confirmation"
   end
 
 end
